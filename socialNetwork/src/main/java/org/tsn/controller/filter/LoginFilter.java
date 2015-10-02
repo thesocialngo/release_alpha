3896f230840ea78dac89d91e6cbd9933f37a85bf
thesocialngo <thesocialngo2015@gmail.com>
2015-10-01 20:31:02 -0400

    Spring filter added to intercept all request - todo authentication check.

--- socialNetwork/src/main/java/org/tsn/controller/filter/LoginFilter.java
+++ socialNetwork/src/main/java/org/tsn/controller/filter/LoginFilter.java
@@ -13,16 +13,25 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.tsn.resources.SessionConstants;
 import org.tsn.tos.UserProfile;
 
-public class LoginFilter extends OncePerRequestFilter {
-	final static Logger logger = Logger.getLogger(LoginFilter.class);
-	 protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-	  
-System.out.println("xxxxxxxxxx");
-		 HttpSession session = request.getSession(true);
-		 UserProfile profile = (UserProfile) session.getAttribute(SessionConstants.USER_PROFILE);
-		 logger.info("user profile found :"+profile);
-		 
+public class LoginFilter extends OncePerRequestFilter
+{
+	final static Logger	logger	= Logger.getLogger(LoginFilter.class);
 
-	                chain.doFilter(request, response);
-	 }
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain chain)
+					throws ServletException,
+					IOException
+	{
+
+		System.out.println("xxxxxxxxxx URI" + request.getRequestURI());
+		HttpSession session = request.getSession(true);
+		UserProfile profile = (UserProfile) session.getAttribute(SessionConstants.USER_PROFILE);
+		logger.info("user profile found :" + profile);
+
+		chain.doFilter(request, response);
 	}
+
+}
